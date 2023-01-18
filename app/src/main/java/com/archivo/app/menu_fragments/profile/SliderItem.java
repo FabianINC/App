b9ce5426d5ef004388be2a60bbe4affaf5061feb
FabianINC <fabiguzmang1205@gmail.com>
2023-01-18 15:05:27 -0600

    Actualizacion

---
+++ app/src/main/java/com/archivo/app/menu_fragments/profile/SliderItem.java
@@ -0,0 +1,20 @@
+package com.archivo.app.menu_fragments.profile;
+
+public class SliderItem {
+
+      private int image;
+
+      SliderItem(int image){
+
+          this.image = image;
+
+      }
+
+      public int getImage(){
+
+          return image;
+
+      }
+
+
+}
