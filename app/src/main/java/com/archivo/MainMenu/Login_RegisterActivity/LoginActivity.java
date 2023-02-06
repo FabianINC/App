7eae7164c247337223ebe9f904b0fa86f1a11b94
andresvm18 <viquezandres1811@gmail.com>
2023-02-06 11:11:06 -0600

    Comentario

--- app/src/main/java/com/archivo/MainMenu/Login_RegisterActivity/LoginActivity.java
+++ app/src/main/java/com/archivo/MainMenu/Login_RegisterActivity/LoginActivity.java
@@ -20,6 +20,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.archivo.Animation.progressDialog;
 import com.archivo.MainMenu.MainActivity;
 import com.archivo.app.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -29,9 +35,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -270,7 +279,7 @@ public class LoginActivity extends AppCompatActivity {
                         startActivity(mainScreen);
 
                     }else{
-                        loadingProgress.dismiss(); // SE QUITA EL PROGRESS DIALOG
+                        //loadingProgress.dismiss(); // SE QUITA EL PROGRESS DIALOG
                         showUnsuccessfulToast();
                     }
 
@@ -346,10 +355,70 @@ public class LoginActivity extends AppCompatActivity {
 
 
 /* ------------------------------------------ FACEBOOK ----------------------------------------- */
+
+    CallbackManager facebookCallBack;
+
     // MÉTODO PARA INICIAR SESIÓN CON FACEBOOK
     private void performFacebookLogin(){
 
+        facebookCallBack = CallbackManager.Factory.create();
+
+        //SE OTORGAN PERMISOS
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
+        LoginManager.getInstance().registerCallback(facebookCallBack,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        handleFacebookAccessToken(loginResult.getAccessToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+
+                    }
+                });
+
+
     }
+
+    /*
+    @Override
+    protected void onActivityResult(int facebookRequestCode, int facebookResultCode, Intent facebookIntent){
+        super.onActivityResult(facebookRequestCode, facebookResultCode, facebookIntent);
+
+        facebookCallBack.onActivityResult(facebookRequestCode, facebookResultCode, facebookIntent);
+    }
+    */
+    FirebaseAuth facebookAuth;
+
+    private void handleFacebookAccessToken(AccessToken facebookToken) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(facebookToken.getToken());
+        facebookAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser facebookUser = facebookAuth.getCurrentUser();
+
+                            //SE HACE EL LLAMADO A OTRA ACTIVIDAD
+                            Intent mainScreen = new Intent(LoginActivity.this, MainActivity.class);
+                            mainScreen.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(mainScreen);
+
+                        } else {
+                           showUnsuccessfulToast();
+                        }
+                    }
+                });
+    }
+
 /* --------------------------------------------------------------------------------------------- */
 
 
